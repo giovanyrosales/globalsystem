cc0dd4c5a8361b9a9122c4ab4d08f6c372d16525
Jonathan <tatangamess@gmail.com>
2023-04-17 09:33:13 -0600

    ajustes notas 2

--- resources/views/backend/menus/superior.blade.php
+++ resources/views/backend/menus/superior.blade.php
@@ -17,6 +17,6 @@
 
     <!--  /globalsystem/admin  -->
 
-    <script type="text/javascript"> var url = "/admin"; </script>
+    <script type="text/javascript"> var url = "/globalsystem/admin"; </script>
 </head>
 <body class="hold-transition sidebar-mini">
