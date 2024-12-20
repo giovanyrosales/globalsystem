20c82cf11fcfd0e03f06d5d886c3fc520215022f
tatangames <54828743+tatangames@users.noreply.github.com>
2024-12-20 09:55:44 -0600

    registro sindicatura nuevas vistasv3

--- resources/views/frontend/login/vistalogin.blade.php
+++ resources/views/frontend/login/vistalogin.blade.php
@@ -138,7 +138,7 @@
 
         // comprasalcaldia.com/admin/login
 
-        axios.post('/admin/login', formData, {
+        axios.post('/comprasalcaldia.com/admin/login', formData, {
         })
             .then((response) => {
                 closeLoading();
