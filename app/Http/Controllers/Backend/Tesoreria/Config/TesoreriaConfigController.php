aa9f215363f19a2f875548cc18638f505401e324
Tatan <tatangamess@gmail.com>
2025-11-07 10:59:00 -0600

    header en garantias1

--- app/Http/Controllers/Backend/Tesoreria/Config/TesoreriaConfigController.php
+++ app/Http/Controllers/Backend/Tesoreria/Config/TesoreriaConfigController.php
@@ -1666,8 +1666,8 @@ class TesoreriaConfigController extends Controller
 
 
 
-        $mpdf = new \Mpdf\Mpdf(['format' => 'LETTER-L']);
-        //$mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER-L']);
+        //$mpdf = new \Mpdf\Mpdf(['format' => 'LETTER-L']);
+        $mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER-L']);
 
        /* $mpdf = new \Mpdf\Mpdf([
             'tempDir' => sys_get_temp_dir(),
