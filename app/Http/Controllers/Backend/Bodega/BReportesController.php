84e1bac386439e1f2a22f95fbc32d543ba0dabea
Tatan <tatangamess@gmail.com>
2025-11-13 11:30:55 -0600

    cambio de titulo8

--- app/Http/Controllers/Backend/Bodega/BReportesController.php
+++ app/Http/Controllers/Backend/Bodega/BReportesController.php
@@ -3003,16 +3003,16 @@ class BReportesController extends Controller
                 <th style='text-align:right; width:6%'>INICIAL</th>
 
                 <!-- columnas $ → 10% -->
-                <th style='text-align:right; width:6%'>$ INICIAL</th>
+                <th style='text-align:right; width:7%'>$ INICIAL</th>
 
                 <th style='text-align:right; width:8%'>ENTRADAS</th>
-                <th style='text-align:right; width:8%'>$ ENTRADAS</th>
+                <th style='text-align:right; width:9%'>$ ENTRADAS</th>
 
                 <th style='text-align:right; width:8%'>SALIDAS</th>
                 <th style='text-align:right; width:8%'>$ SALIDAS</th>
 
                 <th style='text-align:right; width:6%'>SALDO</th>
-                <th style='text-align:right; width:8%'>$ SALDO</th>
+                <th style='text-align:right; width:7%'>$ SALDO</th>
             </tr>
             </thead>
             <tbody>
